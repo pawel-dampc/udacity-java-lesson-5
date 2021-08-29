74c6899078eae044030051da3a39761922363c8c
Pawel <dampc86pawel@gmail.com>
2021-08-29 16:59:59 +0200

    IJ new program

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//Java code for printing out Hello world!
+
+public class HelloWorld {
+    public static void main(String[] args){
+        //write code in here that you want to execute
+        System.out.println("Hello worlf !");
+    }
+}
